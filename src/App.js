144e43bea4ae4d19fb4fc910b5c9a556a55a79a6
Hanna Astrea <dawidgorecny@gmail.com>
2025-11-30 15:50:55 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import { useState } from 'react';
 
 function App() {
+   let nextId = 0;
+   const [fName, setFName] = useState("");
+   const [lName, setLName] = useState("");
+   const [users, setUsers] = useState([]);
+
+   function handleAdd(){
+      if(fName != null && lName != null) {
+         setUsers([
+            ...users,
+            {id: nextId++, firstName: fName, lastName: lName}
+         ]);
+      }
+   }
+
   return (
     <div>
-       <h1>
-        hi
-       </h1>
+      <form>
+         <input type='text' placeholder='Imię'
+         value={fName}
+         onChange={e => setFName(e.target.value)}
+         />
+         <input type='text' placeholder='Nazwisko'
+         value={lName}
+         onChange={e => setLName(e.target.value)}
+         />
+         <button onClick={handleAdd()}>Dodaj nowego użytkownika</button>
+      </form>
+      <hr/>
+      <table>
+         <tr>
+            <th>Id</th>
+            <th>Imię</th>
+            <th>Nazwisko</th>
+         </tr>
+         {users.map(users => (
+            <tr key={users.id}>
+               <td>{users.id}</td>
+               <td>{users.fName}</td>
+               <td>{users.lName}</td>
+            </tr>
+         ))}
+      </table>
     </div>
  );
 }
